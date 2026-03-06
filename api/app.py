3aed0f1fa7de27d678caa2cd823494e3b23eda18
Aasthap3 <porwalaastha@gmail.com>
2026-03-06 22:38:42 +0530

    Fixed cors issue in API

--- api/app.py
+++ api/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import joblib
 
 app = Flask(__name__)
+CORS(app)
 
 model = joblib.load("../model/fake_profile_model.pkl")
 vectorizer = joblib.load("../model/vectorizer.pkl")
@@ -14,10 +16,12 @@ def predict():
     
     X = vectorizer.transform([text])
     prediction = model.predict(X)[0]
+    proba = model.predict_proba(X)[0]
+    confidence = float(max(proba)) * 100
     
     label = "Fake" if prediction == 1 else "Legit"
     
-    return jsonify({"prediction": label})
+    return jsonify({"prediction": label, "confidence": f"{confidence:.2f}%"})
 
 if __name__ == "__main__":
     app.run(debug=True)
